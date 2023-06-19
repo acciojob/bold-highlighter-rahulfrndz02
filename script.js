bf603e84664f166ded9a6329d587ba0e2039cb18
Rahul Kumar Pandit <55670332+rahulfrndz02@users.noreply.github.com>
2023-06-19 11:45:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+var highlight = document.getElementByTagName("strong")
 function highlight() {
     //Write your code here
-
-
+  for(let i=0; i<highlight.length; i++){
+	  highlight[i].style.color = "green"
+  }
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+  for(let i=0; i<highlight.length; i++){
+	  highlight[i].style.color = "black"
+  }
 }
